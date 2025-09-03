e7bd1e633c775fd21422fbcb189171396a9a5f3a
ElcioMFernandes <elcio@elcio.dev>
2025-09-03 00:35:18 -0300

    feat: Added endpoints to search for all trades or by symbol, both with filtering implementation.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,42 @@
-import express, { type Request, type Response } from "express";
+import { filter } from "./utils/filter.js";
 import { socket } from "./services/socket.js";
+import express, { type Request, type Response } from "express";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {});
+app.get("/trades", (req: Request, res: Response): void => {
+  res.json(filter(Object.values(socket.trades).flat(), req.query));
+});
+
+app.get("/trades/:symbol", (req: Request, res: Response): void => {
+  const symbol = String(req.params.symbol).toUpperCase();
+
+  if (socket.trades[symbol]) {
+    res.json(filter(socket.trades[symbol], req.query));
+  } else {
+    res.status(404).json({ message: "Symbol not found" });
+  }
+});
+
+app.get("/trades/:symbol/stats", (req: Request, res: Response): void => {
+  const symbol = String(req.params.symbol).toUpperCase();
+
+  if (socket.trades[symbol]) {
+    res.json({ message: "Method under construction." });
+  } else {
+    res.status(404).json({ message: "Symbol not found" });
+  }
+});
+
+app.get("/trades/:symbol/candle", (req: Request, res: Response): void => {
+  const symbol = String(req.params.symbol).toUpperCase();
+
+  if (socket.trades[symbol]) {
+    res.json({ message: "Method under construction." });
+  } else {
+    res.status(404).json({ message: "Symbol not found" });
+  }
+});
 
 const start = async () => {
   try {
