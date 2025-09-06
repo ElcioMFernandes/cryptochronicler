d56ae586588bfa973c6cdba55158efa46bba2f19
ElcioMFernandes <elcio@elcio.dev>
2025-09-06 12:23:11 -0300

    feat: Adapting methods for using redis

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,41 +1,65 @@
+import express, { type Request, type Response } from "express";
+
 import { filter } from "./utils/filter.js";
 import { socket } from "./services/socket.js";
-import express, { type Request, type Response } from "express";
+
+import type { Trade } from "./types/trade.js";
 
 const app = express();
 
-app.get("/trades", (req: Request, res: Response): void => {
-  res.json(filter(Object.values(socket.trades).flat(), req.query));
-});
+app.get("/trades", async (req: Request, res: Response): Promise<void> => {
+  try {
+    const all: Trade[] = [];
+    for (const key of await socket.redis.keys("*")) {
+      const result = await socket.redis.lRange(key, 0, -1);
 
-app.get("/trades/:symbol", (req: Request, res: Response): void => {
-  const symbol = String(req.params.symbol).toUpperCase();
+      const trades = result.map((trade) => JSON.parse(trade));
 
-  if (socket.trades[symbol]) {
-    res.json(filter(socket.trades[symbol], req.query));
-  } else {
-    res.status(404).json({ message: "Symbol not found" });
+      all.push(...trades);
+    }
+
+    const sorted = all.sort((a, b) => b.T - a.T);
+
+    const filtered = filter(sorted, req.query);
+
+    res.json(filtered);
+  } catch (error) {
+    res.status(500).json({ error: "failure to recover trades" });
   }
 });
 
-app.get("/trades/:symbol/stats", (req: Request, res: Response): void => {
-  const symbol = String(req.params.symbol).toUpperCase();
+app.get(
+  "/trades/:symbol",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const symbol = String(req.params.symbol).toUpperCase();
+      const result = await socket.redis.lRange(symbol, 0, -1);
 
-  if (socket.trades[symbol]) {
+      if (result.length === 0) {
+        res.status(404).json({ message: "Symbol not found" });
+        return;
+      }
+
+      const trades: Trade[] = result.map((trade) => JSON.parse(trade));
+      const sorted = trades.sort((a, b) => b.T - a.T);
+      const filtered = filter(sorted, req.query);
+
+      res.json(filtered);
+    } catch (error) {
+      res.status(500).json({ error: "failure to recover trades for symbol" });
+    }
+  }
+);
+
+app.get(
+  "/trades/:symbol/stats",
+  async (req: Request, res: Response): Promise<void> => {
     res.json({ message: "Method under construction." });
-  } else {
-    res.status(404).json({ message: "Symbol not found" });
   }
-});
+);
 
 app.get("/trades/:symbol/candle", (req: Request, res: Response): void => {
-  const symbol = String(req.params.symbol).toUpperCase();
-
-  if (socket.trades[symbol]) {
-    res.json({ message: "Method under construction." });
-  } else {
-    res.status(404).json({ message: "Symbol not found" });
-  }
+  res.status(501).json({ message: "Method not implemented" });
 });
 
 const start = async () => {
